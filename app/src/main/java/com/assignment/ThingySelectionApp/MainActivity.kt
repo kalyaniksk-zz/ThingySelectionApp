b2c1611d1db3284ac9a023e15e3a59e9f1a9bb78
kalyanikandpile <kalyanikandpile5@gmail.com>
2020-08-27 13:24:19 +0530

    reset adapter in Activity onStop() cycle

--- app/src/main/java/com/assignment/ThingySelectionApp/MainActivity.kt
+++ app/src/main/java/com/assignment/ThingySelectionApp/MainActivity.kt
@@ -3,7 +3,6 @@ package com.assignment.ThingySelectionApp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
